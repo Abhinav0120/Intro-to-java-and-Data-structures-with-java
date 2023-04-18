9b83b8ff531e2f315ed1de0d024ed8fcce1263bf
Abhinav <ajagekarajagekar@gmail.com>
2023-04-19 02:18:57 +0530

    Started revising the java from start

---
+++ introduction_to_java/AverageMarks.java
@@ -0,0 +1,17 @@
+package introduction_to_java;
+
+import java.util.Scanner;
+
+public class AverageMarks {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        char name = s.next().charAt(0);
+        int m1 = s.nextInt();
+        int m2 = s.nextInt();
+        int m3 = s.nextInt();
+        int avg = (m1+m2+m3)/3;
+        System.out.println(name);
+        System.out.println(avg);
+    }
+    
+}
